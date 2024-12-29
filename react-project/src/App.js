2e01dba4f7b9496bea27763e1a60a1625e729eae
ahmad-shanti <s12326330@stu.najah.edu>
2024-12-30 01:47:13 +0200

    add new update

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1> hi my app</h1>
+      <h1> hi my app </h1>
     </div>
   );
 }
---
+++ react-project/src/components/Store.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+
+import storeItems from "../data/items.json";
+import StoreItem from "./StoreItem";
+
+const Store = () => {
+  return (
+    <>
+      <Container className="my-4">
+        <h1 className="mb-4 text-center" >حقائب تراثية فاخرة </h1>
+        <Row md={2} lg={3} xl={4} className="g-4">
+          {storeItems.map((item) => (
+            <Col key={item.id}>
+              <StoreItem {...item} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Store;
