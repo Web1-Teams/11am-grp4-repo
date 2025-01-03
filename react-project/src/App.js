8ca6bceaddd93f07fdcbfc5063dc0f8ca4d843a3
SHADY SAWALHA <s12323788@stu.najah.edu>
2025-01-03 21:18:09 +0200

    تزبيط app js
    تزبيط app js

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
-import StoreItem from "./components/StoreItem";
-
-function App() {
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Store from "./components/Store";
+import About from "./components/About";
+import PaymentConfirmation from "./components/PaymentConfirmation"; 
+import ShoppingCartProvider from "./context/ShoppingCartContext";
+import Thobe from "./components/Thobe";
+import Shalat from "./components/Shalat";
+import Other from "./components/Other";
+import Koffieat from "./components/Koffieat";
+import Jewelry from "./components/Jewelry";
+const App = () => {
   return (
-    <div className="App">
-      <StoreItem></StoreItem>
-    </div>
+    <ShoppingCartProvider>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/payment" element={<PaymentConfirmation />} />
+        <Route path="/thobes" element={<Thobe />} /> 
+        <Route path="/Shalat" element={<Shalat />} /> 
+        <Route path="/other" element={<Other />} /> 
+        <Route path="/koffieat" element={<Koffieat />} /> 
+        <Route path="/jewelry" element={<Jewelry />} /> 
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
   );
-}
+};
 
 export default App;
